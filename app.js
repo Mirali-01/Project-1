69baa54b39a89e604c3b6039c04251791b4d83ac
Mir <mir.m.ali01@gmail.com>
2023-02-21 22:09:24 -0500

    changed button letter feautures

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 // Word Guesser
 
 // Logic Wireframe
-// 1. make alphabet box
-// 2. make input for chosen word (either player can make up a word)
-// 2.1 inputted word should generate blank lines below alphabet box indicating the length of the word
-  // 2.2 check if letters in word are in alphabet
-// 3. select letter from alphabet box to be removed, for either right or wrong
+// * = done
+// 1. make alphabet box *
+// 1.2. select letter from alphabet box to be removed, for either right or wrong *
+// 2. make input for chosen word (either player can make up a word) *
+// 2.1 inputted word should generate blank lines below alphabet box indicated by the length of the word *
+  // 3 check if letters in word are in alphabet
 // 4. if correct letter, then show below on blank lines
 // 5. if incorrect letter, then hangman features are generated, up to 6 features
 // 6. You win if you guess the word correctly
@@ -29,12 +30,6 @@
 // // generate hangman with each incorrect answer
 // const hangmanImg = ["head", "body", "arm 1", "arm 2", "leg 1", "leg 2"];
 
-// if (letter === true) {
-//   console.log(hangmanImg[0]);
-// } else {
-//   console.log(letter);
-// }
-
 
 // inserting player's word
 
@@ -42,8 +37,10 @@ const wordChoice = document.querySelector(".wordChoice")
 const blankSpaces = document.querySelector(".blankSpaces")
 const midScreen = document.querySelector(".midScreen")
 const alphabetLetters = document.querySelector(".alphabet")
+// const letters = document.querySelector(".letter")
+const chooseWord = document.querySelector(".wordChoice")
 
-let chooseWord = () => {
+chooseWord.addEventListener("click", (e) => {
   let input = document.querySelector("input")
   let wordValue = input.value
   console.log(wordValue)
@@ -54,8 +51,18 @@ let chooseWord = () => {
     blankSpaces.append(spaces)
     midScreen.remove()
   }
-}
+})
 
 alphabetLetters.addEventListener("click", (e) => {
   e.target.remove() //target child elements when linked with parent
 })
+
+// letters.addEventListener("click", (e) => {
+//   letters.remove(e)
+// })
+
+
+// 3 check if letters in word are in alphabet
+// let check = () => {
+//   if ()
+// }
