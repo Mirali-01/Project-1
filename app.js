f0ef6ae75625cb3903cc8e3fd44d1e39b39d8823
Mir <mir.m.ali01@gmail.com>
2023-02-23 09:30:52 -0500

    wrong answer test function is working

--- app.js
+++ app.js
@@ -6,7 +6,7 @@
 // 1.2. select letter from alphabet box to be removed, for either right or wrong *
 // 2. make input for chosen word (either player can make up a word) *
 // 2.1 inputted word should generate blank lines below alphabet box indicated by the length of the word *
-  // 3 check if letters in word are in alphabet
+  // 3 check if letters in word are in alphabet *
 // 4. if correct letter, then show below on blank lines
 // 5. if incorrect letter, then hangman features are generated, up to 6 features
 // 6. You win if you guess the word correctly
@@ -19,9 +19,6 @@
 
 // for loop: inputted word save the length of the word chosen and length === # of blank spaces
 
-// letters as button but no alphabetbox
-  // see if letter chosen in word is in word chosen
-  // counter: word.length
 // alphabet
 
 const alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
@@ -32,8 +29,6 @@ const alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P
 // const hangmanImg = ["head", "body", "arm 1", "arm 2", "leg 1", "leg 2"];
 
 
-// inserting player's word
-
 const wrongAnswer = document.querySelector(".stickFigureBox")
 const wordChoice = document.querySelector(".wordChoice")
 const blankSpaces = document.querySelector(".blankSpaces")
@@ -41,6 +36,7 @@ const midScreen = document.querySelector(".midScreen")
 const alphabetLetters = document.querySelector(".alphabet")
 
 
+// inserting player's word
 const chooseWord = document.querySelector(".wordChoice")
 const input = document.querySelector("input")
 
@@ -56,7 +52,7 @@ chooseWord.addEventListener("click", (e) => {
   }
 })
 
-// 3 check if letters in word are in alphabet, this function should go in alphabetLetters event listener
+// 3 check if letters in word are in alphabet, this function should go in alphabetLetters event listener *
 
 const letters = document.querySelectorAll(".letter")
 
@@ -69,22 +65,14 @@ for (let i = 0; i < alphabet.length; i++) {
     if (input.value.split("").includes(alphabet[i].toLowerCase())) {
       console.log(true)
     } else {
-      console.log(false)
+      let hangPiece = document.createElement("div")
+      hangPiece.classList.add("hangPiece")
+      wrongAnswer.append(hangPiece)
     }
     letters[i].remove()
     console.log(alphabet[i])
   })
 }
-
-  
-  //target child elements when linked with parent
-  // let wordContents = input.value.split("")
-  // let alphabetContents = alphabetLetters.textContent.toLowerCase()
-  // if (wordContents.includes(alphabetContents)) {
-  //   console.log("true")
-  // } else {
-  //   e.target.remove() 
-  // }
 
 
 // setTimeout(() => {
