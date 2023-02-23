01665e95961b9642b76d9119a01da64ac47a6dd5
Mir <mir.m.ali01@gmail.com>
2023-02-23 14:56:28 -0500

    cleaned up code a little

--- app.js
+++ app.js
@@ -7,8 +7,8 @@
 // 2. make input for chosen word (either player can make up a word) *
 // 2.1 inputted word should generate blank lines below alphabet box indicated by the length of the word *
   // 3 check if letters in word are in alphabet *
-// 4. if correct letter, then show below on blank lines
-// 5. if incorrect letter, then hangman features are generated, up to 6 features
+// 4. if correct letter, then show below on blank lines *
+// 5. if incorrect letter, then hangman features are generated, up to 6 features *
 // 6. You win if you guess the word correctly
 // 7. You lose if hangman has all features
 // 8. Round ends and next player can input a word
@@ -17,18 +17,10 @@
 // if correct you win, if incorrect you lose instantly
 
 
-// for loop: inputted word save the length of the word chosen and length === # of blank spaces
-
 // alphabet
-
 const alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 
-// console.log(alphabet);
-
-// // generate hangman with each incorrect answer
-// const hangmanImg = ["head", "body", "arm 1", "arm 2", "leg 1", "leg 2"];
-
-
+// query selectors
 const wrongAnswer = document.querySelector(".stickFigureBox")
 const wordChoice = document.querySelector(".wordChoice")
 const blankSpaces = document.querySelector(".blankSpaces")
@@ -36,16 +28,16 @@ const midScreen = document.querySelector(".midScreen")
 const alphabetLetters = document.querySelector(".alphabet")
 
 
-// inserting player's word
+// start by inserting player's word
 const chooseWord = document.querySelector(".wordChoice")
 const input = document.querySelector("input")
 
 let spaceDivs;
-
 let spaces;
 let characters;
-let showLetter;
+let clickLetter;
 
+// checks the input word and generates blank lines for length of word
 chooseWord.addEventListener("click", (e) => {
   let wordValue = input.value
   characters = wordValue.split("")
@@ -63,32 +55,29 @@ chooseWord.addEventListener("click", (e) => {
   }
 })
 
-// 3 check if letters in word are in alphabet, this function should go in alphabetLetters event listener *
 
+// need a function for similar characters
+
+// 3 check if clicked letter is a letter of the alphabet and is a letter in the index of the input word *
 const letters = document.querySelectorAll(".letter")
-
-// letters.forEach(button => {
-//   button.addEventListener("click", () =>  button.remove())
-// })
 
 for (let i = 0; i < alphabet.length; i++) {
   letters[i].addEventListener("click", (e) => {
-    showLetter = e.target.textContent.toLowerCase()
+    clickLetter = e.target.textContent.toLowerCase()
 
+// correct answer for non-similar characters
+if (characters.includes(clickLetter)) {
+  console.log(characters.indexOf(clickLetter))
+  spaceDivs[characters.indexOf(clickLetter)].textContent = clickLetter.toUpperCase()
 
-
-    // let indexArr = []
-// correct answer
-if (characters.includes(showLetter)) {
-  console.log(characters.indexOf(showLetter))
   // for (let i = 0; i < characters.length; i++) {
-    spaceDivs[characters.indexOf(showLetter)].textContent = showLetter.toUpperCase()
+    // spaceDivs[characters.indexOf(clickLetter)].textContent = clickLetter.toUpperCase()
   // }
   
   }
-      
+
     // } else {
-    //   // wrong answer
+    //   // wrong answer function
     //   // 1. create element
     //   let hangPiece = document.createElement("div")
     //   // 2. give it a name
@@ -101,8 +90,19 @@ if (characters.includes(showLetter)) {
   })
 }
 
+
+
+
+
+
+
+
 // setTimeout(() => {
 //   let someFunction = () => {
 
 //   }
 // }, 1000)
+
+// letters.forEach(button => {
+//   button.addEventListener("click", () =>  button.remove())
+// })
