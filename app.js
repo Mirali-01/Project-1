030d8ab64fe61a34eb23160d80dad8d4bceed3db
Mir <mir.m.ali01@gmail.com>
2023-05-25 14:58:54 -0400

    sidebar, navbar, and directions modal created, needs to be more responsive

--- app.js
+++ app.js
@@ -4,10 +4,45 @@
 // Hints in Guess the Word! - give a random letter or first letter in the random word
 // Audio in the background
 // Songs on click or audio on click
-// modal: How to Play
-// Nav: menu, game Logo, modal for how to play
 // Maybe a sign in, maybe???
-// dark theme?
+
+// dark mode
+function myFunction() {
+  var element = document.body;
+  element.classList.toggle("dark-mode");
+}
+
+// Modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal
+btn.onclick = function () {
+  modal.style.display = "block";
+};
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
+
+// sidenav
+var hamburger = document.querySelector(".hamburger");
+hamburger.addEventListener("click", function () {
+  document.querySelector("body").classList.toggle("active");
+});
 
 // default on all games
 let gameOn = true;
